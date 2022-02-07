3873a543d6b673dfdb1ab2387f39ac7cb8df476b
rangerbd <dipto.g1@gmail.com>
2022-02-08 00:18:51 +0600

    laptop

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    
+    return (
+        <div>
+            <h1>this is shop</h1>
+        </div>
+    );
+};
+
+export default Shop;
