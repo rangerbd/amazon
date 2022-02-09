e7da5530414551693d3386d1f9dbb6a73deba221
rangerbd <dipto.g1@gmail.com>
2022-02-09 14:50:52 +0600

    laptop

---
+++ src/components/Product/Product.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const Product = () => {
+
+     const [products ,setProducts] =useState();
+
+     
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Product;
